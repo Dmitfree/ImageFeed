4eddf9247f32a9a5e6a6e18aeb2c352a04bcbda2
Dmitfree <dmitriykhazgeriev@gmail.com>
2023-09-07 22:56:54 +0300

    Спринт_09 - подготовлен экран для верстки User profile.

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//  Created by Dmitfre on 07.09.2023.
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var userProfilePhoto: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userNickNameLabel: UILabel!
+    @IBOutlet weak var userDescriptionLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func userProfileExitButton(_ sender: Any) {
+    }
+}
