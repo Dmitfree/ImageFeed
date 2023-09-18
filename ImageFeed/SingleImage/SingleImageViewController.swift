3b9603d40446e1ed1bbd7f5a0aa910e86b86f97f
Dmitfree <dmitriykhazgeriev@gmail.com>
2023-09-18 21:53:51 +0300

    Финал темы 2

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -3,16 +3,23 @@
 import UIKit
 
 final class SingleImageViewController: UIViewController {
+    
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var backwardButton: UIButton!
+    
     var image: UIImage! {
-            didSet {
-                guard isViewLoaded else { return } // 1
-                imageView.image = image // 2
-            }
+        didSet {
+            guard isViewLoaded else { return } // 1
+            imageView.image = image // 2
         }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+    }
+    
+    @IBAction func backwardButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
 }
