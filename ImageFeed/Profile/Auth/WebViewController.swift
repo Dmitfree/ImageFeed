ad9c5febec91854c0fc0174c26618bf6911c43c8
Dmitfree <dmitriykhazgeriev@gmail.com>
2023-10-09 23:02:41 +0300

    Сверстал кнопку nav_back_button. Следующий шаг - добавляю URLComponents.

--- ImageFeed/Profile/Auth/WebViewController.swift
+++ ImageFeed/Profile/Auth/WebViewController.swift
@@ -1,7 +1,17 @@
 //  Created by Dmitfre on 09.10.2023.
 
 import UIKit
+import WebKit
 
 final class WebViewController: UIViewController {
+    
+    @IBOutlet private var webView: WKWebView!
+    
     private let ShowWebViewSegueIdentifier = "ShowWebView"
+    
+    
+    @IBAction private func didTapBackButton(_ sender: Any) {
+        
+    }
+
 }
