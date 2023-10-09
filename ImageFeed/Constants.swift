04872c20e0ffaca961909332f96e95b540e94fdc
Dmitfree <dmitriykhazgeriev@gmail.com>
2023-10-09 22:03:58 +0300

    Подготовлен View Controller авторизации пользователя.

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,9 @@
+//  Created by Dmitfre on 09.10.2023.
+
+import Foundation
+
+let AccessKey = "kV4TmW-sga0bzUfPrD18YK5IaUwdUuebyvtZCNPAvLg"
+let SecretKey = "MZ3NLHcWtCE-iO6SOb-gXUnbSLueq1qMtbluaU_ztKE"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
